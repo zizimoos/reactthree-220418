927c750e1d1d339812ea830ba97db9546c44fc32
남형석 <azerc@MacMini.local>
2022-04-18 15:01:25 +0900

    ....practice again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useSpring, a } from "@react-spring/three";
+import { Physics, useBox } from "@react-three/cannon";
+
+function Spin({ children }) {
+  const ref = useRef();
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  return <group ref={ref}>{children}</group>;
+}
+
+function Cube(props) {
+  const [active, setActive] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const { scale, color } = useSpring({
+    scale: hovered ? [1, 1, 1] : [0.5, 0.5, 0.5],
+    color: active ? "hotpink" : "dodgerblue",
+  });
+  return (
+    <a.mesh
+      {...props}
+      scale={scale}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <boxGeometry />
+      <a.meshStandardMaterial color={color} />
+    </a.mesh>
+  );
+}
+
+function PCube(props) {
+  const [ref] = useBox(() => ({ mass: 1, ...props }));
+
+  return (
+    <mesh ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial color="teal" />
+    </mesh>
+  );
+}
+
+function Plane(props) {
+  const [ref] = useBox(() => ({ mass: 0, ...props }));
+
+  return (
+    <mesh ref={ref}>
+      <planeGeometry args={[100, 100]} />
+      <meshStandardMaterial color="tomato" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas style={{ width: "600px", height: "600px" }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 15]} />
+      <Spin>
+        <Cube position={[1.5, 1, 1]} />
+      </Spin>
+      <Cube position={[-1.5, 1, 1]} />
+      <Cube position={[-1.5, -1, -1]} />
+      <Physics>
+        <PCube rotation={[-0.4, -10, -5]} />
+        <PCube position={[1.5, 1, 1]} />
+        <PCube position={[-1.9, 1.1, 1.6]} />
+        <Plane rotation={[-Math.PI / 2, 0, 0]} position={[0, -4, 0]} />
+      </Physics>
+      <OrbitControls />
+    </Canvas>
   );
 }
 
